9a17548bfe08437f9e316825904e669b532c349d
Flávio Ribeiro <email@flavioribeiro.com>
2011-05-10 16:54:11 -0300

    challenge 3: backend can rotate and generate screenshot

--- challenge_3/flavyoutube/apps/backend/videos_engine.py
+++ challenge_3/flavyoutube/apps/backend/videos_engine.py
@@ -3,28 +3,60 @@
 
 from subprocess import Popen, PIPE, STDOUT
 import tempfile
+import os
+
+VIDEOS_PATH = "/home/flavio/devel/globaltests/challenge_3/flavyoutube/media/videos/"
 
 class VideoEngine(object):
     def __init__(self, video_path):
         self.video_path = video_path
+        self.extension = self.video_path[self.video_path.rindex("."):]
+        self.pure_name =  self.video_path[self.video_path.rindex("/"):self.video_path.rindex(".")]       
+        self.new_video_path = VIDEOS_PATH + self.pure_name + ".mp4"
 
     def get_screenshot(self):
         screenshot = tempfile.NamedTemporaryFile()
         filename = screenshot.name + ".png"
         screenshot.close()
-	command = '''ffmpeg -i %s -an -ss 00:00:03 -an -r 1 -vframes 1 -s 290x168 %s''' % (self.video_path, filename)
+	command = "ffmpeg -i %s -an -ss 00:00:03 -an -r 1 -vframes 1 -s 290x168 %s" % (self.video_path, filename)
         p = Popen(command.split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
         p.communicate()
         print filename
 
-    def get_encoded_video_path(self):
-        '''ffmpeg -i {0} -y -b 200000 -r 25 -s 320x240 -acodec aac -ab 128kb -vcodec mpeg4 -b 1200kb -mbd 2 -flags +4mv -cmp 2 -subcmp 2 -s 640x390 {1}'''
+    def get_encoded_video_path(self, rotate=False):
+        if rotate:
+            self._rotate_video()
 
-    def rotate_video(self):
-        '''mencoder -oac copy -ovc lavc -vf rotate=1 original.mp4 -o 90_CW_rotated.mp4'''
+        command = "ffmpeg -i %s -acodec copy -vcodec copy %s" % (self.video_path, self.new_video_path)
+        p = Popen(command.split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        p.communicate()
+        print self.new_video_path
 
-a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample1.avi")
-a.get_screenshot()
-a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample2.mpeg")
-a.get_screenshot()
+    def _rotate_video(self):
+        command = "mencoder %s -vf rotate=1 -oac copy -ovc lavc  -o %s" % (self.video_path, "/tmp/" + self.pure_name + "-rotated" + self.extension)
+        p = Popen(command.split(), stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        p.communicate()
+        self.video_path = "/tmp/" + self.pure_name + "-rotated" + self.extension
+       
+if __name__ == "__main__":
+    a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample1.avi")
+    a.get_screenshot()
+    a.get_encoded_video_path(rotate=True)
+    a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample2.mpeg")
+    a.get_screenshot()
+    a.get_encoded_video_path(rotate=True)
+    a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample3.mpg")
+    a.get_screenshot()
+    a.get_encoded_video_path(rotate=True)
+    a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample4.avi")
+    a.get_screenshot()
+    a.get_encoded_video_path(rotate=True)
+    a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample5.avi")
+    a.get_screenshot()
+    a.get_encoded_video_path(rotate=True)
+    a = VideoEngine("/home/flavio/devel/globaltests/challenge_3/flavyoutube/apps/backend/sample6.mpeg")
+    a.get_screenshot()
+    a.get_encoded_video_path(rotate=True)
+
+
 
